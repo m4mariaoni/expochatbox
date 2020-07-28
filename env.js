3bfd0f38d5212af4847c246d782cecefb9f53e8e
Maria Simeon <msimeon@inlaks.com>
2020-07-28 21:50:30 +0100

    initial

---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "chatbox-21-07-2020",
+  private_key_id: "efe1b7998481a0db299ce7d6dd26287dba940e69",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCf0jz/INZhBvB6\n7Vkgdjryjyc1ky/cQ/gSb/es4mH/UCF5O8WUX++bSvmwvWLE1LHItf9MCdd/zZQ8\np6FsQReqrFGtmR4K2kLN5VK0lRAMPElVitSLcgOAd4EJsT9+dOBxj/DUAsn+ckeC\nS0fHmHvlWH+zjc96OCPAHf+ZGAIds6i4HLVpINp827H0+upRv8FMKt3c+NLWQn3/\ngxO6zocOwDutMFX8eQIfssmsjV9Ao6ORG2QlL8NyomhMCfjgeS8w/LnwQbSDA9kt\nhC5hydU3q31tGV8RRU8P7OMhX/DsSSY1we/90OWrcUT4Vcucj6EpDDjLgjgpxtFe\nwv9PxzVdAgMBAAECggEAHGYx4430ujIO+nEBlOSdbeulJn58kzHau3/87VIe4szc\n3duSrN5tR9WHT/IfQmWfH9DDSEI5cmLX8/0/kGpyp6fOLcBXF8uX8llNHHyeaOYv\ni81LHmpImAPkQJIn+7MODO/sdef298lbzpnM6/k7DtJugrvwwcGHDMTn3c7n1VoG\n9Y+cSoz5n6/zYYcO6iFGc7k0S3fTgjjfi/x3106odqr3riueD+2R6d1EjxH413dv\nAh6xu9HMZjScBpP5ZSdJz8lgM+mrax61CNNJOv98WX7kv8KQpys9eW/czjtEaz1m\n6o9/Kp6Sny7DP70XTZwJa/FC120yghLbl3uzXn1lKwKBgQDQvjzVByQekB8t1jcV\nEXxvMAjJWcJg0gAaq7Af+4zRMz3+p0bicLlxWDw6gyvxt7c36Ux7jXFK56sON7Kg\nnCK6zukxIpg2Yz9Yh0MVJ77h05b52xhK4pn15Q9+8VLxmdu4tAOtNrynat96cXzu\n5E2pZAyvGQ3TpOOOSDu6w2rO/wKBgQDEALh5xlf1Z/Kw6RWe/lUizZXaEVFDy/9B\n/mNFyqedzRm/KztorKdPon7CroeD0NKwto3gN47OtxA7M1kMjjqJjQP1uLRP2BRA\npV4xBE0cdIHDRd7/G1DdPGCtjZMh+kUFIjJ3taQ5jaS94vr03Qb/m1kObLxMtLEx\nVya2OlOXowKBgQCvnVzQXBGnJNkWVwJAV1W4tVCvFTTS/9nWllq2jYsK9XFSrKiW\nGP/qu7DwCSsPUOBzyXLu/zAxNHZUn8dIGBOf1jOZguFL4HOKp0FoqueMVdbOneTy\nG3IrzWocAisl16ti2Q++yi4tK1yYRqI02R39RI7nQMzBIhfPvDWECqPxQQKBgACP\nK/eU/E1D9apRj/E7Moz93y6XHA9AdsL+cdQSJvZJToiAHBRJYdTVVt1BWDulDeTZ\nMIBySCMI3K/+joN9qGcaEufRlhogOLaSgjlLlNi+8FvZSDCcKFfpQzFblQ7kwuun\n1FMJHqXXZJ/yFNs8Ajgafl+PKd5iKZtFb2PXuMNXAoGAMbWKLKE5RxBIChGIaPFB\n/eJgfsn/mxLPJlgRKRWCqSkwx8RsdOIOGPqziUiaw/xBf/fLumZ2sykoiVwOOcJA\nkJAkLzxrJ0EA8CYI3BLDov/VkCwJ0dFuFZpm7PoIrfwlvo+Dh/pSEsMH25AbpK5W\nsmt3wjdkGRiKMJByB5eU0GY=\n-----END PRIVATE KEY-----\n",
+  client_email: "dialogflow-rfnvhk@chatbox-21-07-2020.iam.gserviceaccount.com",
+  client_id: "103247180399876427031",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-rfnvhk%40chatbox-21-07-2020.iam.gserviceaccount.com",
+};
